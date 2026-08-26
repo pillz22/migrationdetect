d576dcf4a68b3b05b8707d57381bf6b7fcb4df36
pillz22 <pillz22an22@gmail.com>
2026-08-26 17:09:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ app.get('/fees/:pool', async (req, res) => {
   }
 
   try {
-    const url = `https://api8.axiom.trade/token-info?pairAddress=${pool}&v=${Date.now()}`;
+    const url = `https://api6.axiom.trade/token-info?pairAddress=${pool}&v=${Date.now()}`;
     const response = await fetch(url, {
       headers: {
         'cookie': buildCookie(),
@@ -169,7 +169,7 @@ app.get('/pair-info/:pair', async (req, res) => {
     await refreshAccessToken();
   }
 
-  const url = `https://api9.axiom.trade/pair-info?pairAddress=${pair}&v=${Date.now()}`;
+  const url = `https://api6.axiom.trade/pair-info?pairAddress=${pair}&v=${Date.now()}`;
   const mkHeaders = () => ({
     'cookie': buildCookie(),
     'referer': 'https://axiom.trade/',
